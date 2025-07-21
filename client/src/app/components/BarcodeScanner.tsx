f386088dfad3f5d94041cfc3afc5a82399e1103b
IzeLeam <luca.pourceau@gmail.com>
2025-07-22 00:50:43 +0200

    fix

--- client/src/app/components/BarcodeScanner.tsx
+++ client/src/app/components/BarcodeScanner.tsx
@@ -90,7 +90,6 @@ export default function BarcodeScanner({ onDetected }: { onDetected?: (code: str
     Quagga.onDetected((result: { codeResult: { code: string } }) => {
       const code: string = result.codeResult.code;
       addScannedCode(code);
-      alert(`Code detected: ${code}`);
       const mostProbableCode = getMostProbableCode();
       if (mostProbableCode) {
         stopScanner();
