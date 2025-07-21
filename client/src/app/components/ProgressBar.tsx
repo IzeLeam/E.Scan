2a88bf5b1983d0e2ac702a26df1a1ebac7b8c495
IzeLeam <luca.pourceau@gmail.com>
2025-07-21 19:07:01 +0200

    fix

--- client/src/app/components/ProgressBar.tsx
+++ client/src/app/components/ProgressBar.tsx
@@ -1,12 +1,13 @@
 export default function ProgressBar({ progress }: { progress: number }) {
+  const safeProgress = Math.max(0, Math.min(progress, 100));
   return (
     <>
       <div className="flex justify-between mb-1">
         <span className="text-base font-medium text-blue-700 dark:text-white">Progression</span>
-        <span className="text-sm font-medium text-blue-700 dark:text-white">{progress}%</span>
+        <span className="text-sm font-medium text-blue-700 dark:text-white">{safeProgress}%</span>
       </div>
       <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-        <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${progress}%` }}></div>
+        <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${safeProgress}%` }}></div>
       </div>
     </>
   );
